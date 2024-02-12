af073550d332a0e9e425a9e9a6278dce3d907833
Błażej Kołodziej <blazej4099@gmail.com>
2024-02-12 17:28:04 +0100

    Added the ability to remove items from the order. Minor changes

--- pizzeria/views.py
+++ pizzeria/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic import ListView
+from django.http import JsonResponse
 
 from .models import Pizza, Restaurant, Order, OrderItem
 from .forms import OrderForm, OrderItemForm
@@ -41,25 +44,40 @@ def add_to_order(request, slug):
     context = {
         'pizza': pizza,
         'form': form,
-    } 
+    }
     return render(request, 'add_to_order.html', context)
 
 def order(request):
-    order = request.session.get('order')
-    if order:
+    if request.method == 'GET':
+        if 'order' in request.session:
+            order = sorted(request.session.get('order'), key=lambda d: d['id'])
+            pizza_list = list(Pizza.objects.filter(id__in=[order_item['id'] for order_item in order]).values())
+            for order_item in order:
+                order_item.update(next((pizza for pizza in pizza_list if pizza['id'] == order_item['id']), None))
+                order_item['price'] = order_item['quantity'] * order_item['price_medium'] if order_item['size'] == 'medium' else \
+                                    order_item['quantity'] * order_item['price_large']
+            context = {
+                'order': order,
+                'order_total_price': sum(order_item['price'] for order_item in order)
+            }
+        else:
+            context = {}
+        return render(request, 'order.html', context)
+    
+    elif request.method == 'DELETE':
+        id_item_removed = json.load(request).get('id_item_removed')
+        for order_item in request.session['order']:
+            if order_item['id'] == id_item_removed:
+                request.session['order'].remove(order_item)
+                request.session.modified = True
+        order = sorted(request.session.get('order'), key=lambda d: d['id'])
         pizza_list = list(Pizza.objects.filter(id__in=[order_item['id'] for order_item in order]).values())
-        order.sort(key=lambda d: d['id'])
-        for (order_item, pizza) in zip(order, pizza_list):
-            order_item.update(pizza)
-            order_item['price'] = order_item['quantity'] * order_item['price_medium'] if order_item['size'] == 'medium' else \
-                                  order_item['quantity'] * order_item['price_large']
-        context = {
-            'order': order,
-            'order_total_price': sum(order_item['price'] for order_item in order)
-        }
-    else:
-        context = {}
-    return render(request, 'order.html', context)
+        updated_total_price = 0
+        for order_item in order:
+            order_item.update(next((pizza for pizza in pizza_list if pizza['id'] == order_item['id']), None))
+            updated_total_price += order_item['quantity'] * order_item['price_medium'] if order_item['size'] == 'medium' else \
+                                order_item['quantity'] * order_item['price_large']
+        return JsonResponse({'updated_total_price': updated_total_price})
 
 def order_confirm(request):
     if request.method == 'POST':
